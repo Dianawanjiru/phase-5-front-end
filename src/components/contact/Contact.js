04cf67073466d2c0985101e530567120fda86162
sean-code <john.nganga@student.moringaschool.com>
2022-12-22 18:29:09 +0300

    Create contact component, add form, add styling and set media queries

---
+++ src/components/contact/Contact.js
@@ -0,0 +1,21 @@
+import React from "react";
+import './Contact.css';
+
+
+const Contact = () => {
+    return(
+        <>
+            <div className="contact-container">  
+                <form id="contact" action="" method="post">
+                    <h3>Contact Us</h3>                    
+                    <input className='input1' placeholder="   NAME:" type="text"></input>
+                    <input className='input1' placeholder="   EMAIL:" type="email" tabindex="2" required></input>
+                    <input className='input2' placeholder="   TYPE YOUR MESSAGE:" tabindex="5" required></input>
+                        <button name="submit" type="submit" id="contact-submit" data-submit="...Sending">Submit</button>
+                </form>
+            </div>
+        </>
+    )
+}
+
+export default Contact;
