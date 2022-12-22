acf430fe3c35250972b593f294d107ec7fdd269a
sean-code <john.nganga@student.moringaschool.com>
2022-12-22 15:59:53 +0300

    Create and style the footer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import React from 'react';
 import Navbar from './components/navbar/Navbar';
+import Footer from './components/footer/Footer';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <div className="App">
       <BrowserRouter>
         <Navbar/>
+        <Footer />
       </BrowserRouter>
 
     </div>
