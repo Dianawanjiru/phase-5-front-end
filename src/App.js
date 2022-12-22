df3fec82690cf84de7e6a4937598a89ae16ff2ff
sean-code <john.nganga@student.moringaschool.com>
2022-12-22 14:10:05 +0300

    import BroiwserRouter and add Navbar to the app.js Component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import './App.css';
+import React from 'react';
+import Navbar from './components/navbar/Navbar';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
+        <Navbar/>
+      </BrowserRouter>
+
     </div>
   );
 }
--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import './Navbar.css';
 import { NavLink } from 'react-router-dom';
 
