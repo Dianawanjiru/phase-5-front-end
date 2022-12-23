5e3ae7b99737fe8861520f76f8bd17d9ecf58e50
sean-code <john.nganga@student.moringaschool.com>
2022-12-23 14:50:12 +0300

    import icons and add them using the img tags

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,5 +1,11 @@
 import React from "react";
 import './Footer.css';
+import facebook from './icons/facebook.svg';
+import instagram from './icons/instagram.svg';
+import mail from './icons/mail.svg';
+import locale from './icons/map-pin.svg';
+import phone from './icons/phone.svg';
+import tweet from './icons/twitter.svg';
 
 const Footer = ()=> {
     return (
@@ -23,17 +29,29 @@ const Footer = ()=> {
                     <li>
                         Stalk us on
                         <ul className="f-submenu">
-                            <li>Instagram</li>
-                            <li>Twitter</li>
-                            <li>Meta</li>
+                            <li>
+                                <img src={instagram} alt='instagram'/>{' '}Instagram</li>
+                            <li>
+                                <img src={tweet} alt='tweet'/>{' '}Twitter</li>
+                            <li>
+                                <img src={facebook} alt='meta'/>{' '}Meta</li>
                         </ul>
                     </li>
                     <li>
                         Contact us
                         <ul className="f-submenu">
-                            <li>+97654598765</li>
-                            <li>info@carental.com</li>
-                            <li>Nairobi, KE</li>
+                            <li>
+                                <img src={phone} alt='phone'/>{' '}
+                                +97654598765
+                            </li>
+                            <li>
+                                <img src={mail} alt='mail'/>{' '}
+                                info@carental.com
+                            </li>
+                            <li>
+                                <img src={locale} alt='locale'/>{' '}
+                                Nairobi, KE
+                            </li>
                         </ul>
                     </li>
                 </ul>
