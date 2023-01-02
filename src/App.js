0654315024c2f5099eed7b17c6fad15957684982
sean-code <john.nganga@student.moringaschool.com>
2023-01-02 15:24:00 +0300

    import BrowserRouter as Router

--- src/App.js
+++ src/App.js
@@ -2,17 +2,17 @@ import './App.css';
 import React from 'react';
 import Navbar from './components/navbar/Navbar';
 import Contact from './components/contact/Contact';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
+      <Router>
         <Navbar/>
         <Routes>
           <Route path='/contacts' element={<Contact/>}></Route>
         </Routes>
-      </BrowserRouter>
+      </Router>
 
     </div>
   );
