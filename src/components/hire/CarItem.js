2236502a0a7d26677b399686f8bf36c2d228d0b5
sean-code <john.nganga@student.moringaschool.com>
2023-01-08 03:50:47 +0300

    Create Car Item and pass in attributes as props

---
+++ src/components/hire/CarItem.js
@@ -0,0 +1,42 @@
+import "./Hire.css"
+
+function CarItem({ car }){
+    const {id, price, make, model, fuel_type, transmission, mileage, engine_size, year, image } = car;
+    return(
+            <>
+                <div className="car-item">
+                    <li className="card">
+                        <div className="card_components">
+                            <img src={image} alt={model} />
+                            <div className="details">
+                                <p><strong>{make}, {model}</strong></p>
+                                <p>
+                                    <strong>Price: </strong>US ${price}
+                                </p>
+                                <p>
+                                    <strong>Fuel-Type:</strong>{fuel_type}
+                                </p>
+                                <p>
+                                    <strong>Transmission:</strong>{transmission}
+                                </p>
+                                <p>
+                                    <strong>Mileage:</strong>{mileage}
+                                </p>
+                                <p>
+                                    <strong>Engine-Size:</strong> {engine_size}
+                                </p>
+                                <p>
+                                    <strong>Year:</strong>{year}
+                                </p>
+                                <button> Hire </button>
+                                <button> Details </button>
+                            </div>
+                        </div>
+                    </li>
+                </div>
+            </>
+    )
+
+}
+
+export default CarItem;
