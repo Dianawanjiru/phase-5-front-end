5a14270b20f02933fa4c22b3d6c5ff880bdb1316
sean-code <john.nganga@student.moringaschool.com>
2023-01-13 10:30:21 +0300

    Create the main parent that handles payment choice options and style it

--- src/components/pages/payment/AllPayments.js
+++ src/components/pages/payment/AllPayments.js
@@ -1,9 +1,43 @@
+import './AllPayment.css';
+import { Link } from 'react-router-dom';
+import carsData from '../../data/CarsData';
+import { useParams } from 'react-router-dom';
+import Navbar from '../../navbar/Navbar';
+import Footer from '../../footer/Footer';
 
 const AllPayment = () => {
+    const { slug } = useParams();
+    const singleCarItem = carsData.find((item) => item.make === slug);
 
     return(
         <>
-            
+            <Navbar />
+            <div className='mn-container'>
+                <div className='btn-container'>
+                <Link to={`/cars/${singleCarItem.make}/payment/mpesa`}>
+                    <a className='btn-links' href={`/cars/${singleCarItem.make}/payment/mpesa`}>
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                        Pay Via M-Pesa
+                    </a>
+                </Link>
+                   
+                    <Link to={`/cars/${singleCarItem.make}/payment/card`}>
+                        <a className= 'btn-links' href={`/cars/${singleCarItem.make}/payment/card`}>
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                            Pay Via Card
+                        </a>
+                    </Link>
+                </div>
+            </div>
+            <Footer />
         </>
     )
 }
+
+export default AllPayment;
