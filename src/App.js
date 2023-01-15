08637276f3ae342c3d62970b1b52c9d393439826
Richard Ouma <richard.ouma@student.moringaschool.com>
2023-01-15 11:15:10 +0300

    Changin the tite's name

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import { useEffect, useState,React } from 'react';
-import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+// import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Login from './Components/Login';
-import ResetPasswordForm from './Components/PswdresetForm';
-import SignUpForm from './Components/SignupForm';
+
 
 function App() {
   const [user, setUser] = useState(null);
@@ -21,13 +20,7 @@ function App() {
 
   return (
 <>
-<Router>
-  <Routes>
-  <Route path='/' element={<Login/>}></Route>
-  <Route path='/ForgotPassword' element={<ResetPasswordForm/>}></Route>
-  <Route path='/signup' element={<SignUpForm/>}></Route>
-  </Routes>
-</Router>
+{/*routes goes here */}
 </>
 
   );
