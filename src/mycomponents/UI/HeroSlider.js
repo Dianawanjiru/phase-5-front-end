e400bf4887f255f7256faeead5a92735ed1774ea
Diana <diana.wanjiru@student.moringaschool.com>
2023-01-16 17:58:20 +0300

    worked on the reserve buton

--- src/mycomponents/UI/HeroSlider.js
+++ src/mycomponents/UI/HeroSlider.js
@@ -2,11 +2,13 @@ import React from "react";
 
 import Slider from "react-slick";
 import { Container } from "reactstrap";
+
 import { Link } from "react-router-dom";
 
 import "../../mystyles/hero-slider.css"
 
 const HeroSlider = () => {
+  
   const settings = {
     fade: true,
     speed: 2000,
@@ -24,10 +26,12 @@ const HeroSlider = () => {
           <div className="slider__content ">
             <h4 className="text-light mb-3">For Rent $70 Per Day</h4>
             <h1 className="text-light mb-4">Reserve Now and Get 50% Off</h1>
-
+            <Link to="/services/hire">
             <button className="btn reserve__btn mt-4">
-              <Link to="/cars">Reserve Now</Link>
+              
+              Reserve Now
             </button>
+            </Link> 
           </div>
         </Container>
       </div>
@@ -38,9 +42,12 @@ const HeroSlider = () => {
             <h4 className="text-light mb-3">For Rent $70 Per Day</h4>
             <h1 className="text-light mb-4">Reserve Now and Get 50% Off</h1>
 
+            <Link to="/services/hire">
             <button className="btn reserve__btn mt-4">
-              <Link to="/cars">Reserve Now</Link>
+              
+              Reserve Now
             </button>
+            </Link> 
           </div>
         </Container>
       </div>
@@ -51,9 +58,12 @@ const HeroSlider = () => {
             <h4 className="text-light mb-3">For Rent $70 Per Day</h4>
             <h1 className="text-light mb-4">Reserve Now and Get 50% Off</h1>
 
+            <Link to="/services/hire">
             <button className="btn reserve__btn mt-4">
-              <Link to="/cars">Reserve Now</Link>
+              
+              Reserve Now
             </button>
+            </Link> 
           </div>
         </Container>
       </div>
