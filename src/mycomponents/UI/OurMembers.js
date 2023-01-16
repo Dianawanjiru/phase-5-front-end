9a82d03be7702a7da6d2fefe5f42c2cd75194c43
Diana <diana.wanjiru@student.moringaschool.com>
2023-01-16 18:29:49 +0300

    chenged the words to English

--- src/mycomponents/UI/OurMembers.js
+++ src/mycomponents/UI/OurMembers.js
@@ -2,13 +2,13 @@ import React from "react";
 import "../../mystyles/our-member.css";
 import { Col } from "reactstrap";
 import { Link } from "react-router-dom";
-import ava01 from "../../assets/all-images/ava-1.jpg";
+import ava01 from "../../assets/all-images/Lin.jpeg";
 import ava02 from "../../assets/all-images/ava-2.jpg";
 import ava03 from "../../assets/all-images/ava-3.jpg";
 
 const OUR__MEMBERS = [
   {
-    name: "Jhon Doe",
+    name: "Linah Imbari",
     experience: "5 years of experience",
     fbUrl: "#",
     instUrl: "#",
